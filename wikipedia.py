997755f3c89ef4b680ec3d7c2f0fc174ea4d5f73
kbandla <kbandla@outlook.com>
2025-06-21 10:47:18 -0400

    Updates
    - Add basic .md report
    - Add get summary
    - Add logging, argparse

--- wikipedia.py
+++ wikipedia.py
@@ -6,9 +6,20 @@ A small python wrapper for Wikipedia API
 """
 from pprint import pprint
 from datetime import datetime
+import logging
 import requests
 
+logger = logging.getLogger('wikipedia')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter2 = logging.Formatter('%(levelname)s-%(name)s %(message)s')
+ch = logging.StreamHandler()
+ch.setFormatter(formatter2)
+logger.addHandler(ch)
+logger.setLevel(logging.INFO)
+
 URL = "https://en.wikipedia.org/w/api.php"
+URL_USER = "https://en.wikipedia.org/wiki/User:"
+URL_ARTICLE= "https://en.wikipedia.org/wiki/"
 
 class Wikipedia:
     def __init__(self, debug=False):
@@ -20,6 +31,8 @@ class Wikipedia:
         Performs a request
         :return:
         """
+        if 'format' not in params:
+            params["format"] = "json"
         response = self.session.get(url=URL, params=params)
         return response.json()
 
@@ -33,14 +46,14 @@ class Wikipedia:
         keys = ['uclimit', 'ucstart', 'ucend', 'uccontinue', 'ucuser', 'ucuserids', 'ucuserprefix', 'ucnamespace', 'ucshow', 'uctag', 'uctoponly']
         params = {
             "action": "query",
-            "format": "json",
             "list": "usercontribs",
         }
         # insert any other values from kwargs
         for key in keys:
             if key in kwargs:
                 params[key] = kwargs.get(key)
-
+        username = params.get('ucuser')
+        logger.debug(f"Fetching user contributions for {username}..")
         data = self.get_json(params)
         queryD = data.get('query')
         usercontribsL = queryD.get('usercontribs')
@@ -53,10 +66,18 @@ class Wikipedia:
             if timestamp.year not in stats_year:
                 stats_year[timestamp.year] = []
             stats_year[timestamp.year].append(title)
-        print('Showing Stats for %s'%params.get('ucuser'))
+        logger.debug(f'Showing Stats for {username}')
+        self.outfile.write(f'# Stats for {username}\n')
+        self.outfile.write(f'| Username | Articles |\n| -- | -- |\n')
+        for year in stats_year:
+            self.outfile.write(f"| {year} | ")
+            for article in stats_year.get(year):
+                self.outfile.write(f'[{article}](<{URL_ARTICLE}{article}>)<br>')
+            self.outfile.write(' |\n')
+        self.outfile.write('\n\n')
         if self.debug:
             pprint(stats_year)
-
+            pprint('-'*110)
         batchcompleteB = bool(data.get('batchcomplete'))
         continueD = data.get('continue', {})
         uccontinue = continueD.get('uccontinue')
@@ -76,7 +97,7 @@ class Wikipedia:
                   "prop": "revisions",
                   "rvprop": "timestamp|user|comment|size",
                   "rvslots": "main",
-                  "formatversion": "2", "format": "json",
+                  "formatversion": "2", 
                   "titles": titles,
                   "rvlimit": 500}
         # insert any other values from kwargs
@@ -88,7 +109,7 @@ class Wikipedia:
         batchcompleteB = bool(data.get('batchcomplete'))
         queryD = data.get('query')
         pagesL = queryD.get('pages')
-        print('Query returned %s pages'%len(pagesL))
+        logger.debug('Query returned %s pages'%len(pagesL))
         stats_year = {}
         stats_user = {}
         for num, page in enumerate(pagesL):
@@ -105,19 +126,83 @@ class Wikipedia:
                 if user not in stats_user:
                     stats_user[user] = 0
                 stats_user[user] += 1
-        xx = sorted(stats_user.items(), key=lambda x: x[1], reverse=True)
+        userstats_sorted = sorted(stats_user.items(), key=lambda x: x[1], reverse=True)
+        title = ''.join(titles)
+        # Report yearly stats
+        self.outfile.write(f'# Yearly stats for {title}\n')
+        self.outfile.write(f'| year | Users |\n| -- | -- |\n')
+        for year in stats_year:
+            self.outfile.write(f'| {year} | ')
+            for user in stats_year.get(year):
+                self.outfile.write(f'[{user}](<{URL_USER}{user}>)<br>')
+            self.outfile.write(' |\n')
+        self.outfile.write('\n\n')
+        self.outfile.write(f'# Stats by username for {title}\n')
+        self.outfile.write(f'| Username | Revisions |\n| -- | -- |\n')
+        # Report user stats (sorted)
+        for name, revcount in userstats_sorted:
+            self.outfile.write(f"| [{name}](<{URL_USER}{name}>) | {revcount} |\n")
+        self.outfile.write('\n\n')
         if self.debug:
             pprint(stats_year)
-            pprint(xx[:10])
+            pprint(userstats_sorted[:10])
         return stats_user
 
-    def analyze_page(self, title):
-        results = self.revisions(title)
-        for username, num in results:
+    def analyze_page(self, titles):
+        filename = ''.join(titles).replace(' ','')
+        self.outfile = open(f'{filename}_report.md', 'w')
+        title = ''.join(titles)
+        self.outfile.write(f"# Wikipedia Analysis : {title}\n\n")
+        summary = self.get_lead(titles)
+        self.outfile.write(f"{summary}\n\n")
+        results = self.revisions(titles)
+        for username, num in results.items():
             self.usercontribs(ucuser=username)
 
+    def get_lead(self, titles):
+        """
+        Get the lead section for a page in plaintext 
+        
+        NOTE: response is like
+        {   'batchcomplete': '',
+            'query': {
+                'normalized': [{'from': 'pizza', 'to': 'Pizza'}],
+                'pages': {
+                    '24768': {'extract': 'Pizza is an Italian, specifically '
+        """
+        params = {
+            'action' : 'query',
+            'prop': 'extracts',
+            'exintro': True,
+            'explaintext': True,
+            'titles': titles,
+        }
+        data = self.get_json(params)
+        batchcompleteB = bool(data.get('batchcomplete'))
+        queryD = data.get('query')
+        pagesD = queryD.get('pages')
+        logger.debug('Query returned %s pages'%len(pagesD))
+        for num, extractD in pagesD.items():
+            extract = extractD.get('extract')
+            logger.debug(extract)
+        return extract
 
 if __name__ == "__main__":
-    import sys
+    import argparse
+    parser = argparse.ArgumentParser(description='Wikipedia Search')
+    parser.add_argument('-v', '--verbose', action='store_true', help="Increase output verbosity")
+    parser.add_argument('-a', '--analyze', action='store_true', help='Analyze edits to a page')
+    parser.add_argument('--summary', action='store_true', help='Get summary for a page')
+    parser.add_argument('titles', nargs='*', action='store', help='Search query')
+    args = parser.parse_args()
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+    logger.debug(args)
     wiki = Wikipedia()
-    wiki.analyze_page(sys.argv[1])
+    wiki.debug = args.verbose
+    if args.analyze:
+        wiki.analyze_page(args.titles)
+    elif args.summary:
+        summary = wiki.get_lead(args.titles)
+    else:
+        parser.print_help()
